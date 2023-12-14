16798a4d61fd0b6090e696fb835e296be928b42d
Filipe Silveira <filipe.silveira@ua.pt>
2023-12-14 16:52:44 +0000

    Finished top maze. Corrected player script

--- scripts/KinematicBody.gd
+++ scripts/KinematicBody.gd
@@ -1,45 +1,63 @@
 extends KinematicBody
 
-var velocity = Vector3.ZERO
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-const SPEED = 5
+# Speed of the player movement.
+var speed: float = 10.0
+# Jump power.
+var jump_power: float = 5.0
+# Sensitivity of the mouse movement.
+var mouse_sensitivity: float = 0.05
+# Gravity value.
+var gravity: float = -9.8
+# Velocity of the player.
+var velocity: Vector3 = Vector3.ZERO
+# To keep track of whether the player is on the ground.
+var on_ground: bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass  # Replace with function body.
+	# Capture the mouse cursor.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	var direction = Vector3.ZERO
+# Override '_input' to handle mouse motion events and ESC key.
+func _input(event):
+	if event is InputEventMouseMotion:
+		# Mouse movement for camera rotation.
+		var mouse_movement = event.relative * mouse_sensitivity
+		rotate_y(deg2rad(-mouse_movement.x))
+		$Camera.rotate_x(deg2rad(-mouse_movement.y))
+		$Camera.rotation.x = clamp($Camera.rotation.x, deg2rad(-90), deg2rad(90))
 
-	# Adjusted movement directions
-	if Input.is_action_pressed("ui_right"):
-		direction += Vector3.RIGHT
-	elif Input.is_action_pressed("ui_left"):
-		direction += Vector3.LEFT
+	# Close the game if ESC is pressed.
+	if event is InputEventKey and event.pressed and event.scancode == KEY_ESCAPE:
+		get_tree().quit()
 
-	if Input.is_action_pressed("ui_up"):
-		direction += Vector3.FORWARD
-	elif Input.is_action_pressed("ui_down"):
-		direction += Vector3.BACK
+# Called every physics frame (fixed frame rate).
+func _physics_process(delta: float):
+	on_ground = is_on_floor()
 
-	# Normalize direction to avoid faster diagonal movement
-	direction = direction.normalized()
+	var forward = Vector3.ZERO
+	var right = Vector3.ZERO
 
-	# Apply movement and rotation
-	if direction != Vector3.ZERO:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	# WASD movement.
+	if Input.is_key_pressed(KEY_W): #UP
+		forward -= transform.basis.z
+	if Input.is_key_pressed(KEY_S): #DOWN
+		forward += transform.basis.z
+	if Input.is_key_pressed(KEY_A): #LEFT
+		right -= transform.basis.x
+	if Input.is_key_pressed(KEY_D): #Right
+		right += transform.basis.x
 
-		# Calculate target rotation
-		var target_rotation = atan2(direction.x, direction.z)
-		rotation.y = lerp_angle(rotation.y, target_rotation, 0.1)
-	else:
-		velocity.x = 0
-		velocity.z = 0
+	var direction = (forward + right).normalized()
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
 
-	# Apply gravity
-	velocity.y -= gravity * delta
+	# Apply gravity.
+	velocity.y += gravity * delta
 
-	# Perform the move
-	velocity = move_and_slide(velocity)
+	# Jump mechanic.
+	if on_ground and Input.is_action_just_pressed("ui_accept"): # assuming ui_accept is the space bar
+		velocity.y = jump_power
+
+	# Move the player.
+	velocity = move_and_slide(velocity, Vector3.UP)
