1893ee086c68fed709aa2b916e7a0d383f987eaf
Filipe Silveira <filipe.silveira@ua.pt>
2023-12-27 22:31:14 +0000

    Added main menu with instructions and about

---
+++ scripts/menu.gd
@@ -0,0 +1,27 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/Level1.tscn")
+
+func _on_instructions_pressed():
+	get_tree().change_scene_to_file("res://scenes/instructions_menu.tscn")
+
+func _on_about_pressed():
+	get_tree().change_scene_to_file("res://scenes/about_menu.tscn")
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+
